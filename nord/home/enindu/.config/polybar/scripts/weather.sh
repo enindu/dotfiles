95310a739dce7b3c94d71f6ad128ff4db60da6ee
Enindu Alahapperuma <enindu@gmail.com>
2020-01-21 08:12:24 +0530

    Update Nord rice

---
+++ nord/home/enindu/.config/polybar/scripts/weather.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Original source - https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/openweathermap-detailed
+
+# Global settings
+KEY="1484ecc8401352a242d0abc88bc194f6"
+CITY="Homagama, LK"
+UNITS="metric"
+SYMBOL="°C"
+API="https://api.openweathermap.org/data/2.5"
+
+# Get weather
+WEATHER=$(curl -sf "$API/weather?APPID=$KEY&q=$CITY&units=$UNITS")
+
+# Get condition, icon and temp
+WEATHER_MAIN=$(echo $WEATHER | jq -r ".weather[0].main")
+WEATHER_TEMP=$(echo $WEATHER | jq -r ".main.temp")
+
+# Print weather
+echo "$WEATHER_MAIN $WEATHER_TEMP$SYMBOL"
